cb244d3be36a43c18daf6265fbbbde23321d3e44
kl-botsu <botsu@khonsulabs.com>
2021-11-01 14:14:46 +0000

    Deploying to gh-pages from @ khonsulabs/englishid@a437240f9af25608499cc2f890df363fed418825 🚀

--- main/implementors/core/iter/traits/iterator/trait.Iterator.js
+++ main/implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["englishid"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"englishid/struct.EnglishId.html\" title=\"struct englishid::EnglishId\">EnglishId</a>","synthetic":false,"types":["englishid::EnglishId"]}];
+implementors["englishid"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"englishid/struct.EnglishId.html\" title=\"struct englishid::EnglishId\">EnglishId</a>","synthetic":false,"types":["englishid::EnglishId"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
